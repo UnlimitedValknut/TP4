da5c31415122233f425a5540555b4e7331328d15
Damian Alladio <damian.e.alladio@gmail.com>
2017-06-13 19:41:08 -0300

    Estructura Básica de Carpetas

---
+++ test/grafo/MatrizSimetricaTest.java
@@ -0,0 +1,17 @@
+package grafo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatrizSimetricaTest {
+
+	private static String archivoIn = "Preparacion de Prueba/Lote de Prueba/Entrada/";
+	private static String archivoOut = "Ejecucion de Prueba/Salida Obtenida/";
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
