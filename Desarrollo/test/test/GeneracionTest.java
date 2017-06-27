0b183e82706150daa222b5cdca4ea009085f843b
Pablo Sebastián García <pablosebas33@gmail.com>
2017-06-27 15:47:11 -0300

    Acomodando los generadores para el test

--- Desarrollo/test/test/GeneracionTest.java
+++ Desarrollo/test/test/GeneracionTest.java
@@ -3,38 +3,34 @@ package test;
 import org.junit.Test;
 
 import grafo.GeneradorDeGrafos;
+import grafo.GrafoNDNP;
 
 public class GeneracionTest {
 	private static final String PATH_ARCHIVOS = "Preparacion de Prueba/Programa Probador/Entrada/";
 
 	@Test
 	public void generarGrafoAleatorioNyPorcAdy600y40() {
-		GrafoNDNP grafo = GeneradorDeGrafos.generarAleatorioNYPorcAdyacencia(600, 40.0);
-		grafo.grabarEntradaGrafo(PATH_ARCHIVOS + "grafo600y40.in");
+		GeneradorDeGrafos.generarGrafoAleatorioPorcentajeAdyacencia(PATH_ARCHIVOS + "grafo600y40.in", 600, 40);
 	}
 
 	@Test
 	public void generarGrafoAleatorioNyPorcAdy600y60() {
-		GrafoNDNP grafo = GeneradorDeGrafos.generarAleatorioNYPorcAdyacencia(600, 60.0);
-		grafo.grabarEntradaGrafo(PATH_ARCHIVOS + "grafo600y60.in");
+		GeneradorDeGrafos.generarGrafoAleatorioPorcentajeAdyacencia(PATH_ARCHIVOS + "grafo600y60.in",600, 60);
 	}
 
 	@Test
 	public void generarGrafoAleatorioNyPorcAdy600y90() {
-		GrafoNDNP grafo = GeneradorDeGrafos.generarAleatorioNYPorcAdyacencia(600, 90.0);
-		grafo.grabarEntradaGrafo(PATH_ARCHIVOS + "grafo600y90.in");
+		GeneradorDeGrafos.generarGrafoAleatorioPorcentajeAdyacencia(PATH_ARCHIVOS + "grafo600y90.in",600, 90);
 	}
 
 	@Test
 	public void generarGrafoRegular1000y75() {
-		GrafoNDNP grafo = GeneradorDeGrafos.generarRegularNYAdyacencia(1000, 75.0);
-		grafo.grabarEntradaGrafo(PATH_ARCHIVOS + "grafo1000y75.in");
+		GeneradorDeGrafos.generarGrafoRegularPorcentajeAdyacencia(PATH_ARCHIVOS + "grafo1000y75.in",1000, 75);
 	}
 
 	@Test
 	public void generarGrafoRegular1000y50() {
-		GrafoNDNP grafo = GeneradorDeGrafos.generarRegularNYAdyacencia(1000, 50.0);
-		grafo.grabarEntradaGrafo(PATH_ARCHIVOS + "grafo1000y50.in");
+		GeneradorDeGrafos.generarGrafoRegularPorcentajeAdyacencia(PATH_ARCHIVOS + "grafo1000y50.in",1000, 50);
 	}
 
 	@Test
